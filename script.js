eb61980b1aff0292d77cbb260cee96e965761cbb
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-11-25 14:38:58 +0100

    move js to separate file

---
+++ script.js
@@ -0,0 +1,58 @@
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
+
+function init() {
+ loadJSON(function(response) {
+  // Parse JSON string into object
+    var data = JSON.parse(response).data;
+    console.log(data)
+    labels
+    // var all = actual_JSON.data
+    var x_values = data.map((line) => line[0])
+    var y_values = data.map((line) => line[1])
+    debugger;
+    // console.log('hello');
+    // console.log(actual_JSON);
+ });
+}
+
+init();
+
+// var getRandomDataArray = function () {
+//   var dataArray = [];
+//   for (var i = 0; i < 7; i++) dataArray.push(Math.round(Math.random() * 100));
+//   return dataArray;
+// }
+
+var chartData = {
+  labels : ["January","February","March","April","May","June","July"],
+  datasets : [
+    {
+      label: "My First dataset",
+      fillColor : "rgba(220,220,220,0.2)",
+      strokeColor : "rgba(220,220,220,1)",
+      pointColor : "rgba(220,220,220,1)",
+      pointStrokeColor : "#fff",
+      pointHighlightFill : "#fff",
+      pointHighlightStroke : "rgba(220,220,220,1)",
+      data : getRandomDataArray()
+    }
+  ]
+}
+
+window.onload = function(){
+  var chartOptions = { responsive : true };
+  var chart = document.getElementById("canvas").getContext("2d");
+  window.myBar = new Chart(chart).Line(chartData, chartOptions);
+}
